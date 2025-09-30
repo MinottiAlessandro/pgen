03fe1658eb78725e38deba6e661d9c7ec796a7fa
Alessandro Minotti <alessandro_minotti@yahoo.it>
2025-09-30 18:26:51 +0200

    Various fix

--- pgen.c
+++ pgen.c
@@ -2,10 +2,10 @@
 #include <stdlib.h>
 #include <string.h>
 
-char lower[] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z', '\0'};
-char upper[] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z', '\0'};
-char digits[] = {'0','1','2','3','4','5','6','7','8','9', '\0'};
-char special[] = {' ','!','"','#','$','%','&','\'','(',')','*','+',',','-','.','/',':',';','<','=','>','?','@','[','\\',']','^','_','`','{','|','}','~', '\0'};
+char lower[] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','\0'};
+char upper[] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','\0'};
+char digits[] = {'0','1','2','3','4','5','6','7','8','9','\0'};
+char special[] = {' ','!','"','#','$','%','&','\'','(',')','*','+',',','-','.','/',':',';','<','=','>','?','@','[','\\',']','^','_','`','{','|','}','~','\0'};
 
 typedef struct Options {
     char exclude[128];
@@ -17,12 +17,6 @@ typedef struct Options {
     unsigned int len;
     char *custom_alphabet;
 } Options;
-
-unsigned int slen(char *s) {
-    unsigned int i = 0;
-    while(s[i] != '\0') ++i;
-    return i;
-}
 
 void print_help() {
     printf("pgen usage:\
@@ -36,8 +30,42 @@ void print_help() {
     \n\t-h\tDisplay this help page.\n");
 }
 
+void error_handler(int err_no) {
+    switch(err_no) {
+        case 1: printf("Error generating the random string.\n"); break;
+        case 2: printf("Impossible to create a password, no alphabet to work on.\n"); break;
+        case 3: printf("Add an argument to specify the password lenght.\n"); break;
+        case 4: printf("The string length must be an integer greater than 0.\n"); break;
+        case 5: printf("Specify a custom alphabet.\n"); break;
+        default: return;
+    }
+    print_help();
+    exit(-1);
+}
+
+unsigned int slen(char *s) {
+    unsigned int i = 0;
+    while(s[i] != '\0') ++i;
+    return i;
+}
+
+void scat(char *buffer, char *alpha, char *exclude) {
+    if(!exclude) {
+        strcat(buffer, alpha);
+        return;
+    }
+
+    int j = slen(buffer);
+    for(unsigned int i = 0; i < slen(alpha); i++) {
+        if(exclude[(unsigned int) alpha[i]]) continue;
+        buffer[j] = alpha[i];
+        ++j;
+    }
+
+}
+
 int parse_argument(Options *opt, int argc, char *argv[]) {
-    if(argc < 2) return -1;
+    if(argc < 2) return 3;
 
     for(int i = 1; i < argc - 1; i++) {
         int skip = 0;
@@ -50,12 +78,12 @@ int parse_argument(Options *opt, int argc, char *argv[]) {
                 case 'd': opt->digits = 1; break;
                 case 's': opt->special = 1; break;
                 case 'c': 
-                    if(argc - i <= 1) return -3;
+                    if(argc - i <= 1) return 5;
                     opt->custom_alphabet = argv[i+1];
                     skip = 1;
                     break;
                 case 'x': 
-                    if(argc - i <= 1) return -3;
+                    if(argc - i <= 1) return 5;
                     int z = 0;
                     while(argv[i+1][z] != '\0') {
                         opt->exclude[argv[i+1][z] % 128] = 1;
@@ -80,7 +108,7 @@ int parse_argument(Options *opt, int argc, char *argv[]) {
     }
 
     int len = atoi(argc[argv-1]);
-    if(len <= 0) return -2;
+    if(len <= 0) return 4;
     opt->len = (unsigned int) len;
     return 0;
 }
@@ -88,57 +116,15 @@ int parse_argument(Options *opt, int argc, char *argv[]) {
 char* build_alphabet(Options *opt) {
     char *buffer;
     buffer = (char *) malloc(sizeof(char) * 96);
-    if(!buffer) {
-        printf("Error generating the random string.\n");
-        exit(1);
-    }
+    
+    if(!buffer) error_handler(1);
 
     for(int i = 0; i < 96; i++) buffer[i] = '\0';
 
-    if(opt->lower) {
-        if(!opt->exclude) strcat(buffer, lower);
-        else {
-            int j = slen(buffer);
-            for(unsigned int i = 0; i < slen(lower); i++) {
-                if(opt->exclude[(unsigned int) lower[i]]) continue;
-                buffer[j] = lower[i];
-                ++j;
-            }
-        }
-    }
-    if(opt->upper) {
-        if(!opt->exclude) strcat(buffer, upper);
-        else {
-            int j = slen(buffer);
-            for(unsigned int i = 0; i < slen(upper); i++) {
-                if(opt->exclude[(unsigned int) upper[i]]) continue;
-                buffer[j] = upper[i];
-                ++j;
-            }
-        }
-    }
-    if(opt->digits) {
-        if(!opt->exclude) strcat(buffer, digits);
-        else {
-            int j = slen(buffer);
-            for(unsigned int i = 0; i < slen(digits); i++) {
-                if(opt->exclude[(unsigned int) digits[i]]) continue;
-                buffer[j] = digits[i];
-                ++j;
-            }
-        }
-    }
-    if(opt->special) {
-        if(!opt->exclude) strcat(buffer, special);
-        else {
-            int j = slen(buffer);
-            for(unsigned int i = 0; i < slen(special); i++) {
-                if(opt->exclude[(unsigned int) special[i]]) continue;
-                buffer[j] = special[i];
-                ++j;
-            }
-        }
-    }
+    if(opt->lower) scat(buffer, lower, opt->exclude);
+    if(opt->upper) scat(buffer, upper, opt->exclude);
+    if(opt->digits) scat(buffer, digits, opt->exclude);
+    if(opt->special) scat(buffer, special, opt->exclude);
 
     return buffer;
 }
@@ -148,25 +134,19 @@ void generate_password(char *p, Options *opt) {
     unsigned int i = 0;
     char *alphabet;
 
-    if(f == 0) {
-        printf("Error generating the random string.\n");
-        exit(1);
-    }
+    if(f == 0) error_handler(1);
     
     srand((unsigned int) fgetc(f));
     fclose(f);
 
     alphabet = build_alphabet(opt);
 
-    if(slen(alphabet) == 0) {
-        printf("Impossible to create a password, no alphabet to work on.\n");
-        exit(0);
-    }
+    if(slen(alphabet) == 0) error_handler(2);
 
     while(i < opt->len) {
         unsigned int rnd = rand();
         
-        char c = alphabet[rnd % (slen(alphabet) - 1)];
+        char c = alphabet[rnd % slen(alphabet)];
 
         p[i++] = c;
     }
@@ -179,24 +159,7 @@ int main(int argc, char* argv[]) {
     char *p;
     Options opt = {{0}, 0, 0, 0, 0, 0, 0, NULL};
 
-    switch(parse_argument(&opt, argc, argv)) {
-        case 1:
-            print_help();
-            return 0;
-        case -1:
-            printf("Add an argument to specify the password lenght.\n");
-            print_help();
-            return -1;
-        case -2:
-            printf("The string length must be an integer greater than 0.\n");
-            print_help();
-            return -2;
-        case -3:
-            printf("Specify a custom alphabet.\n");
-            print_help();
-            return -3;
-        default: break;
-    }
+    error_handler(parse_argument(&opt, argc, argv));
     
     p = (char *) malloc((sizeof(char) * opt.len) + 1);
     generate_password(p, &opt);
