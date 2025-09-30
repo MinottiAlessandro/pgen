66e697207dae00a7d8a0cf0cdfc58e73671ef308
Alessandro Minotti <alessandro_minotti@yahoo.it>
2025-09-30 14:35:46 +0200

    General and parsing argument function improved

--- README.md
+++ README.md
@@ -14,7 +14,7 @@ You can easily *build*, *install*, *clean* and *uninstall* the binary by calling
 - Start optimize the logic, some of the techniques might be:
     - Hardcode and dinamically build the alphabet
     - Extract a buffer of n bytes from /dev/random and work with it
-    - Better flag parsing
+    - ~~Better flag parsing~~
     - Add O(1) exclude filter
     - Take advantage of multicore CPUs systems
     - Could be interesting to build a binary without dependencies
--- pgen.c
+++ pgen.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 typedef struct Options {
-    unsigned int alpha;
-    unsigned int up_low;
+    unsigned int upper;
+    unsigned int lower;
     unsigned int digits;
     unsigned int special_char;
     unsigned int help;
@@ -13,10 +12,15 @@ typedef struct Options {
     char *exclude;
 } Options;
 
+unsigned int slen(char *s) {
+    unsigned int i = 0;
+    while(s[i] != '\0') ++i;
+    return i;
+}
+
 void print_help() {
     printf("pgen usage:\
     \npgen [opt] <int> [the parameters past the integer are ignored]\
-    \n\t-a\tInclude all alpha characters.\
     \n\t-u\tInclude uppercase letters.\
     \n\t-l\tInclude lowercase letters.\
     \n\t-d\tInclude digits.\
@@ -27,85 +31,64 @@ void print_help() {
 }
 
 unsigned int in(char c, char *s) {
-    for(int i = 0; i < strlen(s); i++) {
+    for(unsigned int i = 0; i < slen(s); i++) {
         if(c == s[i]) return 1;
     }
     return 0;
 }
 
 int parse_argument(Options *opt, int argc, char *argv[]) {
-    
-    int len = -1;
-
     if(argc < 2) return -1;
 
-    for(int i = 1; i < argc; i++) {
-        if(strcmp(argv[i], "-a") == 0) {
-            opt->alpha = 1;
-            continue;
-        }
-        if(strcmp(argv[i], "-u") == 0) {
-            opt->up_low = 1;
-            continue;
-        }
-        if(strcmp(argv[i], "-l") == 0) {
-            opt->up_low = 2;
-            continue;
-        }
-        if(strcmp(argv[i], "-d") == 0) {
-            opt->digits = 1;
-            continue;
-        }
-        if(strcmp(argv[i], "-s") == 0) {
-            opt->special_char = 1;
-            continue;
-        }
-        if(strcmp(argv[i], "-c") == 0) {
-            if(argc - i <= 1) {
-                return -3;
+    for(int i = 1; i < argc - 1; i++) {
+        int skip = 0;
+        int j = 1;
+        while(argv[i][j] != '\0') {
+            switch(argv[i][j]) {
+                case '-': ++j; break;
+                case 'u': opt->upper = 1; break;
+                case 'l': opt->lower = 1; break;
+                case 'd': opt->digits = 1; break;
+                case 's': opt->special_char = 1; break;
+                case 'c': 
+                    if(argc - i <= 1) return -3;
+                    opt->custom_alphabet = argv[i+1];
+                    skip = 1;
+                    break;
+                case 'x': 
+                    if(argc - i <= 1) return -3;
+                    opt->exclude = argv[i+1];
+                    skip = 1;
+                    break;
+                case 'h': return 1;
+                default: return 1;
             }
-            opt->custom_alphabet = (char *) malloc(strlen(argv[i+1]) * sizeof(char));
-            strcpy(opt->custom_alphabet, argv[i+1]);
-            ++i;
-            continue;
+            ++j;
         }
-        if(strcmp(argv[i], "-x") == 0) {
-            if(argc - i <= 1) {
-                return -3;
-            }
-            opt->exclude = (char *) malloc(strlen(argv[i+1]) * sizeof(char));
-            strcpy(opt->exclude, argv[i+1]);
-            ++i;
-            continue;
-        }
-        if(strcmp(argv[i], "-h") == 0) {
-            print_help();
-            return 0;
-        }
-        if((len = atoi(argv[i])) <= 0) {
-            return -2;
-        } else {
-            return len;
-        }
+
+        if(skip) ++i;
     }
 
-    return len;
+    int len = atoi(argc[argv-1]);
+    if(len <= 0) return -2;
+    opt->len = (unsigned int) len;
+
+    return 0;
 }
 
 int truth_machine(char c, Options *opt) {
     return (
         ((c >= 48 && c <= 57) && opt->digits) ||
-        ((c >= 65 && c <= 90) && opt->up_low == 1) ||
-        ((c >= 97 && c <= 122) && opt->up_low == 2) ||
-        (((c >= 65 && c <= 90) || (c >= 97 && c <= 122)) && opt->alpha) ||
+        ((c >= 65 && c <= 90) && opt->upper == 1) ||
+        ((c >= 97 && c <= 122) && opt->lower == 1) ||
         (((c >= 32 && c <= 47) ||(c >= 58 && c <= 64) ||(c >= 91 && c <= 96) ||(c >= 123 && c <= 126)) && opt->special_char) ||
-        ((!opt->alpha) && (!opt->up_low) && (!opt->special_char) && (!opt->digits) && (!opt->custom_alphabet))
+        ((!opt->upper) && (!opt->lower) && (!opt->special_char) && (!opt->digits) && (!opt->custom_alphabet))
     );
 }
 
 void generate_password(char *p, Options *opt) {
     FILE *f = fopen("/dev/urandom", "rb");
-    int i = 0;
+    unsigned int i = 0;
 
     if(f == 0) {
         printf("Error generating the random string.\n");
@@ -115,7 +98,7 @@ void generate_password(char *p, Options *opt) {
     while(i < opt->len) {
         char c = fgetc(f);
 
-        if(opt->custom_alphabet) c = opt->custom_alphabet[c % strlen(opt->custom_alphabet)];
+        if(opt->custom_alphabet) c = opt->custom_alphabet[c % slen(opt->custom_alphabet)];
         else {
             c = c % 128;
             if((c < 32 || c > 126) || !truth_machine(c, opt)) continue;
@@ -132,9 +115,12 @@ void generate_password(char *p, Options *opt) {
 int main(int argc, char* argv[]) {
 
     char *p;
-    Options opt = {0, 0, 0, 0, 0, 0, NULL};
+    Options opt = {0, 0, 0, 0, 0, 0, NULL, NULL};
 
-    switch(opt.len = parse_argument(&opt, argc, argv)) {
+    switch(parse_argument(&opt, argc, argv)) {
+        case 1:
+            print_help();
+            return 0;
         case -1:
             printf("Add an argument to specify the password lenght.\n");
             print_help();
@@ -157,8 +143,6 @@ int main(int argc, char* argv[]) {
     printf("%s\n", p);
 
     free(p);
-    if(opt.custom_alphabet) free(opt.custom_alphabet);
-    if(opt.exclude) free(opt.exclude);
 
     return 0;
 }
